75f55d1daa72076e9cbce1680743d3b31fa12d12
H2Owater425 <h2o@h2owr.xyz>
2023-06-07 21:06:39 +0900

    feat: Implement temporary redirection page

---
+++ script.js
@@ -0,0 +1,17 @@
+(function () {
+	const remainTimeText = document.getElementById('remainTimeText');
+	let remainTime = 5;
+
+	const intervalId = window.setInterval(function () {
+		remainTimeText['textContent'] = String(--remainTime);
+		
+		if(remainTime === 0) {
+			window.clearInterval(intervalId);
+			window['location']['href'] = 'https://github.com/isegye-dimigo/';
+		}
+		
+		return;
+	}, 1000);
+
+	return;
+})();
